506e75c5f5766ad31392b14d97b7e8082c132e38
codymart1986 <codymart1986@hotmail.com>
2021-11-09 19:10:15 -0800

    added api endpoints for songs to getAll, getById, updateSong, deleteSong, addSong

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const repoContext = require('./repository/repository-wrapper');
 const { validateProduct } = require('./middleware/products-validation');
 const cors = require('cors');
+const { validateSong } = require('./middleware/songs-validation');
 
 const app = express();
 
@@ -40,5 +41,36 @@ app.put('/api/products/:id', [validateProduct], (req, res) => {
 app.delete('/api/products/:id', (req, res) => {
     const id = req.params.id;
     const updatedDataSet = repoContext.products.deleteProduct(id);
+    return res.send(updatedDataSet);
+});
+
+// Beginning of API endpoints for songs
+app.get('/api/songs', (req, res) => {
+    const songs = repoContext.products.findAllSongs();
+    return res.send(songs);
+});
+
+app.get('/api/songs/:id', (req, res) => {
+    const id = req.params.id;
+    const song = repoContext.songs.findSongById(id);
+    return res.send(song);
+});
+
+app.post('/api/songs', [validateSong], (req, res) => {
+    const newSong = req.body;
+    const addedSong = repoContext.songs.createSong(newSong);
+    return res.send(addedSong);
+});
+
+app.put('/api/songs/:id', [validateSong], (req, res) => {
+    const id= req.params.id;
+    const songPropertiesToUpdate = req.body;
+    const updatedSong = repoContext.songs.updateSong(id, songPropertiesToUpdate);
+    return res.send(updatedSong);
+});
+
+app.delete('/api/songs/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedDataSet = repoContext.songs.deleteSong(id);
     return res.send(updatedDataSet);
 });
